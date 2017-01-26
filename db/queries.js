2a688b8fd34c996b1ba52b631e91e5ac5b367ae9
Ann Robson <nosbora@gmail.com>
2017-01-26 10:21:50 -0800

    very simple query to local database. shows json, which isn't correct but we can use this to test AWS.

---
+++ db/queries.js
@@ -0,0 +1,130 @@
+var promise = require('bluebird');
+
+var options = {
+  // Initialization Options
+  promiseLib: promise
+};
+
+var connectionString;
+var connectionObject;
+
+var pgp = require('pg-promise')(options);
+// LOCALHOST:
+connectionString = 'postgres://localhost:5432/amb';
+
+// or with connctionObject;
+// connectionObject = {
+//     host: 'localhost',
+//     port: 5432,
+//     database: 'amb',
+//     user: 'arobson'
+// };
+
+// AWS:
+connectionObject = {
+    host: 'ambdbinstance.crufdsximznc.us-west-1.rds.amazonaws.com',
+    port: 5432,
+    database: 'amb',
+    user: 'arobson',
+    password: 'h34rt4nn71'
+};
+
+var db = pgp(connectionString || connectionObject);
+var fs = require('fs');
+
+// add query functions
+
+function getAllBikes(req, res, next) {
+  db.any('select * from bike')
+    .then(function (data) {
+      res.status(200)
+        .json({
+          status: 'success',
+          data: data,
+          message: 'Retrieved ALL bikes'
+        });
+    })
+    .catch(function (err) {
+      return next(err);
+    });
+}
+
+function getSingleBike(req, res, next) {
+  var bikeID = parseInt(req.params.id);
+  db.one('select * from bike where id = $1', bikeID)
+    .then(function (data) {
+      res.status(200)
+        .json({
+          status: 'success',
+          data: data,
+          message: 'Retrieved ONE bike'
+        });
+    })
+    .catch(function (err) {
+      return next(err);
+    });
+}
+
+function createBike(req, res, next) {
+  var previewPath = req.body.preview_path;
+  db.one('insert into bike(user_id, style, brand, model) ' +
+      'values($1, $2, $3, $4) returning id', [parseInt(req.body.user_id), req.body.style, req.body.brand, req.body.model])
+    .then(function (data) {
+      var bikeId = data.id;
+      var newDir = 'dist/images/mock/';
+      var extension = previewPath.split('.')[1];
+      var newPath = newDir+data.id+'.'+extension;
+      fs.rename(previewPath, newPath, function() {
+        console.log('successfully moved preview image');
+      });
+      res.status(200)
+        .json({
+          status: 'success',
+          bikeId: bikeId
+        });
+    })
+    .catch(function (err) {
+      return next(err);
+    });
+}
+
+function updateBike(req, res, next) {
+  db.none('update bike set style=$1, brand=$2, model=$3, nickname=$4 where id=$5',
+    [req.body.style, req.body.brand, req.body.model,
+      req.body.nickname, parseInt(req.params.id)])
+    .then(function () {
+      res.status(200)
+        .json({
+          status: 'success',
+          message: 'Updated bike'
+        });
+    })
+    .catch(function (err) {
+      return next(err);
+    });
+}
+
+function removeBike(req, res, next) {
+  var bikeID = parseInt(req.params.id);
+  db.result('delete from bike where id = $1', bikeID)
+    .then(function (result) {
+      /* jshint ignore:start */
+      res.status(200)
+        .json({
+          status: 'success',
+          message: `Removed ${result.rowCount} bike`
+        });
+      /* jshint ignore:end */
+    })
+    .catch(function (err) {
+      return next(err);
+    });
+}
+
+module.exports = {
+  getAllBikes: getAllBikes,
+  getSingleBike: getSingleBike,
+  createBike: createBike,
+  updateBike: updateBike,
+  removeBike: removeBike
+};
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,36 @@
 var express = require('express');
 var router = express.Router();
+const pg = require('pg');
+const path = require('path');
+const connectionString = process.env.DATABASE_URL || 'postgres://localhost:5432/amb';
+
+router.get('/', (req, res, next) => {
+  const results = [];
+  // Get a Postgres client from the connection pool
+  pg.connect(connectionString, (err, client, done) => {
+    // Handle connection errors
+    if(err) {
+      done();
+      console.log(err);
+      return res.status(500).json({success: false, data: err});
+    }
+    // SQL Query > Select Data
+    const query = client.query('SELECT * FROM bike;');
+    // Stream results back one row at a time
+    query.on('row', (row) => {
+      results.push(row);
+    });
+    // After all data is returned, close connection and return results and show index page)
+    query.on('end', () => {
+      done();
+      res.render('index', { title: 'About My Bike Home', bikes: res.json(results) });
+    });
+  });
+});
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+// router.get('/', function(req, res, next) {
+//   res.render('index', { title: 'Express' });
+// });
 
 module.exports = router;
