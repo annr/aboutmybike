537af3e3f224e7afc6a52a65332ca93f4ee5f172
Ann Robson <nosbora@gmail.com>
2017-01-26 14:22:11 -0800

    make a switch for connection if env is dev

--- routes/index.js
+++ routes/index.js
@@ -2,33 +2,22 @@ var express = require('express');
 var router = express.Router();
 const pg = require('pg');
 const path = require('path');
-//const connectionString = process.env.DATABASE_URL || 'postgres://localhost:5432/amb';
 
-// just curious if this works.
-
-const connectionObject = {
-    host: 'ambpublicinstance.crufdsximznc.us-west-1.rds.amazonaws.com',
-    port: 5432,
-    database: 'amb',
-    user: 'arobson',
-    password: 'h34rt4nn71'
-};
-
-/*
-const connectionObject = {
+let connection = {
     host: process.env.RDS_HOSTNAME,
     port: process.env.RDS_PORT,
     database: process.env.RDS_DB_NAME,
     user: process.env.RDS_USERNAME,
     password: process.env.RDS_PASSWORD
 };
-*/
-
 
 router.get('/', (req, res, next) => {
+  if(req.app.get('env') === 'development') {
+    connection = 'postgres://localhost:5432/amb';
+  }
   const results = [];
   // Get a Postgres client from the connection pool
-  pg.connect(connectionObject, (err, client, done) => {
+  pg.connect(connection, (err, client, done) => {
     // Handle connection errors
     if(err) {
       done();
