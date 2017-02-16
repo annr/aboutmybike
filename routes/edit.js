e9be114d887f53efe235e50492ff2332edfc78df
Ann Robson <nosbora@gmail.com>
2017-02-16 12:44:33 -0800

    add some content to Specifics form. remove active class because defaulting this was is not going to add first tab to pushstate. use form-control for color input and simplify overridden style slightly. add boostrap custom-form styles for inline form fields.

--- routes/edit.js
+++ routes/edit.js
@@ -6,56 +6,57 @@ const queries = require('../db/queries');
 const helper = require('../helpers/bike');
 let page_heading =  'Let\'s make your bike page!';
 let page_title = 'Add Your Bike';
-var rows = 10;
+let rows = 10;
+let view = 'edit';
 
-/* GET upload form. */
-router.get('/', function(req, res, next) {
-  res.render('edit', {
-    app_name: res.locals.app.name,
-    page_title: page_title,
-    page_heading: page_heading,
-    reasons: helper.getFormReasons(),
-    types: helper.getFormTypes(),
-    rows: rows
-  });
-});
-
-router.get('/:id', function(req, res, next) {
-
-  let id = req.params.id;
-  let page_title = 'Edit Bike Details';
-  let page_heading = page_title;
+router.get(['/', '/:id'], function(req, res, next) {
   var maxRows = 24;
   var line = 60;
   var calculatedRows;
 
-  queries.getBike(id, function(err, data) {
-    if(err) {
-      next(err);
-    } else {
+  if (!req.params.id) {
+    console.log(page_heading);
+    res.render(view, {
+      app_name: res.locals.app.name,
+      page_title: page_title,
+      page_heading: page_heading,
+      reasons: helper.getFormReasons(),
+      types: helper.getFormTypes(),
+      rows: rows
+    });
+  } else {
+    let id = req.params.id;
+    page_title = 'Edit Bike Details';
+    page_heading = page_title;
 
-      data.photo_url = res.locals.app.s3Url + data.main_photo_path;
+    queries.getBike(id, function(err, data) {
+      if(err) {
+        next(err);
+      } else {
 
-      // Try to guess how many textarea rows will be necessary to edit the text. 
-      // This is hard, because responsive, but if the text is really long, you can give more editor.
-      if(data.description.length) {
-        calculatedRows = Math.round(data.description.length/line);
-        if(calculatedRows < maxRows && calculatedRows > rows) {
-          rows = calculatedRows;
+        data.photo_url = res.locals.app.s3Url + data.main_photo_path;
+
+        // Try to guess how many textarea rows will be necessary to edit the text.
+        // This is hard, because responsive, but if the text is really long, you can give more editor.
+        if(data.description.length) {
+          calculatedRows = Math.round(data.description.length/line);
+          if(calculatedRows < maxRows && calculatedRows > rows) {
+            rows = calculatedRows;
+          }
         }
-      }
 
-      res.render('edit', {
-        app_name: res.locals.app.name,
-        page_title: page_title,
-        page_heading: page_heading,
-        bike: data,
-        reasons: helper.getFormReasons(data.reason_ids),
-        types: helper.getFormTypes(data.type_id),
-        rows: rows
-      });
-    }
-  });
+        res.render(view, {
+          app_name: res.locals.app.name,
+          page_title: page_title,
+          page_heading: page_heading,
+          bike: data,
+          reasons: helper.getFormReasons(data.reason_ids),
+          types: helper.getFormTypes(data.type_id),
+          rows: rows
+        });
+      }
+    });
+  }
 
 });
 
