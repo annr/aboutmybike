ed27bbd0d971a93fd298d86ffff56c8d92278945
Ann Robson <nosbora@gmail.com>
2017-03-20 08:11:02 -0700

    test relative callback URL. Not sure why the examples use full urls.

--- auth/oauth.js
+++ auth/oauth.js
@@ -2,12 +2,12 @@ let ids = {
   production: {
     clientID: process.env.FACEBOOK_APP_ID,
     clientSecret: process.env.FACEBOOK_APP_SECRET,
-    callbackURL: 'http://aboutmybike.com/auth/facebook/callback'
+    callbackURL: '/auth/facebook/callback',
   },
   localhost: {
     clientID: '395199327523676',
     clientSecret: 'c7b483a37c9fa5d74b9c258c2d469fe6',
-    callbackURL: 'http://localhost:3000/auth/facebook/callback'
+    callbackURL: '/auth/facebook/callback',
   }
 };
 
