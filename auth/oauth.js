9bb307c15d3a9a640c65d9543d267c1841cca35c
Ann Robson <nosbora@gmail.com>
2017-03-20 09:35:34 -0700

    can't use relative path for fb auth callback, as it turns out. set it from environment settings, unless local.

--- auth/oauth.js
+++ auth/oauth.js
@@ -2,12 +2,12 @@ let ids = {
   production: {
     clientID: process.env.FACEBOOK_APP_ID,
     clientSecret: process.env.FACEBOOK_APP_SECRET,
-    callbackURL: '/auth/facebook/callback',
+    callbackURL: process.env.FACEBOOK_AUTH_CALLBACK_URL,
   },
   localhost: {
     clientID: '395199327523676',
     clientSecret: 'c7b483a37c9fa5d74b9c258c2d469fe6',
-    callbackURL: '/auth/facebook/callback',
+    callbackURL: 'http://localhost:3000/auth/facebook/callback'
   }
 };
 
