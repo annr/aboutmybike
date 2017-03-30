f711f36ec7e5f3dfe11e103201f49d67ecfe711f
Ann Robson <nosbora@gmail.com>
2017-03-29 19:51:18 -0700

    needed to save connect-flash to package.json. don't show bikes in gallery until user is verified.

--- db/sql/bike/all.sql
+++ db/sql/bike/all.sql
@@ -9,6 +9,9 @@
           bike_info.era AS era
      FROM bike b 
 LEFT JOIN bike_info ON b.id = bike_info.bike_id
+LEFT JOIN amb_user ON b.user_id = amb_user.id
 LEFT JOIN photo ON b.main_photo_id = photo.id
 LEFT JOIN type on b.type_ids[1]=type.id
-    WHERE b.description IS NOT null AND b.status != -1
+    WHERE b.description IS NOT null 
+      AND b.status != -1
+      AND amb_user.verified = true
